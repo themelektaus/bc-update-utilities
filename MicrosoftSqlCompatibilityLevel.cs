9dd9d08945fc60bd8d43bbfa40f1050ef0f32e45
MelekTaus Moribundus <melektaus@outlook.at>
2024-04-29 23:37:06 +0200

    Apply Sql Compatibiliy Level

---
+++ MicrosoftSqlCompatibilityLevel.cs
@@ -0,0 +1,11 @@
+﻿
+enum MicrosoftSqlCompatibilityLevel
+{
+    Unknown = 0,
+    SqlServer2008 = 100,
+    SqlServer2012 = 110,
+    SqlServer2014 = 120,
+    SqlServer2016 = 130,
+    SqlServer2017 = 140,
+    SqlServer2019 = 150
+}
